74cdad23d7af676696c1e6f510b53e4a2c341fb7
LKNGKAYE <32047238+LKNGKAYE@users.noreply.github.com>
2017-09-22 23:42:09 +0800

    final 2342

--- javascript lesson/bottles.js
+++ javascript lesson/bottles.js
@@ -4,7 +4,7 @@
 // 1 bottle of beer on the wall, 1 bottle of beer.
 // Go to the store and buy some more, 99 bottles of beer on the wall.
 
-document.write("WELCOME");
+
 
 // 90-70
 // var a=90
@@ -19,21 +19,24 @@ var a = "";
 
 for (a = 99; a > 0; a--) {
     if (a > 2) {
-        msg += a + " bottles of beer on the wall, " + a + " bottles of beer." + "<br>" + "Take one down and pass it around," + a - 1 + " bottles of beer on the wall.";
+        msg += a + " bottles of beer on the wall, " + a + " bottles of beer. <br> Take one down and pass it around," + (a - 1) + " bottles of beer on the wall.<br><br>";
     } else if (a == 2) {
-        msg += a + " bottles of beer on the wall, " + a + " bottles of beer." + "<br>" + "Take one down and pass it around," + a - 1 + " bottle of beer on the wall.";
+        msg += a + " bottles of beer on the wall, " + a + " bottles of beer. <br> Take one down and pass it around," + (a - 1) + " bottle of beer on the wall.<br><br>";
     } else {
-        msg += a + " bottles of beer on the wall, " + a + " bottles of beer." + "<br>" + "Take one down and pass it around," + a - 1 + " bottle of beer on the wall.";
+        msg += a + " bottles of beer on the wall, " + a + " bottles of beer. <br> Go to the store and buy some more, 99 bottles of beer on the wall.";
     }
 }
 document.write(msg);
+console.log(msg);
+
+
 
 //     if (a > 2) {
 //         msg1 += a + " bottles of beer on the wall," + a + " bottles of beer. Take one down and pass it around," + a - 1 + " bottles of beer on the wall."
 //     } else if (a == 2) {
 //         msg2 += a + " bottles of beer on the wall," + a + " bottles of beer. Take one down and pass it around," + a - 1 + " bottle of beer on the wall."
 //     } else {
-//         msg3 += a + " bottles of beer on the wall," + (a) + " bottles of beer. Go to the store and buy some more," + a + 99 + "bottles of beer on the wall."
+//         msg3 += a + " bottles of beer on the wall," + (a) + " bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
 //     }
 // }
 
